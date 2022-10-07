fa4b9ad7a9a87effa8243b48198c8adb11d7b105
multihaxzors <43578117+multihaxzors@users.noreply.github.com>
2022-10-07 17:22:52 -0400

    Removed Lab Testing Content

--- app/app.js
+++ app/app.js
@@ -34,7 +34,6 @@ app.use(express.urlencoded({ extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(session({
-    secret: Secret,
     saveUninitialized: false,
     resave: false
 }));
